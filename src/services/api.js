7c9ec00d8ea7577d5e53aa0824f0363b5590cf6e
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-05-22 11:10:59 -0300

    Update ip

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://192.168.100.170:3333',
+  baseURL: 'http://192.168.31.101:3333',
 })
 
 export default api
+
+
+// json-server ./src/services/server.json --host 192.168.31.101 --port 3333 --delay 600 
