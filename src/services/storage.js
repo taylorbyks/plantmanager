05e596291ae6172360987f659c01c41a19740e44
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-05-23 01:54:52 -0300

    Add new functions

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { format } from 'date-fns'
 
 export async function savePlant(plant) {
   try {
@@ -27,7 +28,22 @@ export async function loadPlant() {
     const data = await AsyncStorage.getItem('@plantmanager:plants')
     const plants = data ? JSON.parse(data) : {}
 
-    return plants
+    const plantsSorted = Object
+      .keys(plants)
+      .map((plant) => {
+        return {
+          ...plants[plant].data,
+          hour: format(new Date(plants[plant].data.dateTimeNotification), 'HH:mm'),
+        }
+      })
+      .sort((a, b) => 
+        Math.floor(
+          new Date(a.dateTimeNotification).getTime() / 100 
+            - Math.floor(new Date(b.dateTimeNotification).getTime() / 1000),
+        )
+      )
+
+    return plantsSorted;
   } catch(error) {
     throw new Error(error)
   }
