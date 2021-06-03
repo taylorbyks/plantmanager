68f427b73cc2518bbbae9397d7d3f7d35b6af2e8
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-06-02 22:47:17 -0300

    Create danger button

--- src/components/atoms/IconButton/index.js
+++ src/components/atoms/IconButton/index.js
@@ -3,9 +3,9 @@ import { IconButtonContainer } from './styles'
 
 import Icon from 'react-native-vector-icons/Ionicons'
 
-export const IconButton = ({ iconName, onPress }) => {
+export const IconButton = ({ iconName, onPress, danger }) => {
   return (
-    <IconButtonContainer onPress={onPress}>
+    <IconButtonContainer onPress={onPress} danger={danger}>
       <Icon name={iconName} color="white" size={30} />
     </IconButtonContainer>
   )
--- src/components/atoms/IconButton/styles.js
+++ src/components/atoms/IconButton/styles.js
@@ -1,13 +1,15 @@
 import styled, { css } from 'styled-components/native'
 
 export const IconButtonContainer = styled.TouchableOpacity`
-  ${({ theme }) => css`
-    width: ${theme.metrics.px(56)}px;
-    height: ${theme.metrics.px(56)}px;
+  ${({ theme, danger }) => css`
+    width: ${danger ? theme.metrics.px(120) : theme.metrics.px(56)}px;
+    height: ${danger ? theme.metrics.px(109) : theme.metrics.px(56)}px;
+    margin-top: ${danger ? theme.metrics.px(10) : theme.metrics.px(0)}px;
+    margin-right: ${danger ? theme.metrics.px(5) : theme.metrics.px(0)}px;
     display: flex;
     align-items: center;
     justify-content: center;
-    border-radius: ${theme.metrics.px(16)}px;
-    background-color: ${theme.colors.green};
+    border-radius: ${danger ? theme.metrics.px(20) : theme.metrics.px(16)}px;
+    background-color: ${danger ? theme.colors.red : theme.colors.green};
   `}
 `
