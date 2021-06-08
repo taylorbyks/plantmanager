aad4233f1539003a7435e4561c1b3e5f30b82fcb
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-06-07 21:41:35 -0300

    Add notifications and load user

--- App.js
+++ App.js
@@ -1,17 +1,46 @@
-import { StatusBar } from "expo-status-bar"
-import React from "react";
-import { ThemeProvider } from "styled-components"
-import { theme } from "./src/styles/themes"
-import AppLoading from "expo-app-loading"
-import { useFonts, Jost_400Regular, Jost_600SemiBold } from "@expo-google-fonts/jost"
-import Routes from "./src/routes";
-
+import { StatusBar } from 'expo-status-bar'
+import React, { useEffect, useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './src/styles/themes'
+import AppLoading from 'expo-app-loading'
+import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost'
+import { Routes, AuthRoutes } from './src/routes'
+import * as Notifications from 'expo-notifications'
+import { loadUser } from './src/services/storage'
 
 export default function App() {
-  const [ FontsLoaded ] = useFonts({ Jost_400Regular, Jost_600SemiBold })
-  
-  if(!FontsLoaded){
+  const [FontsLoaded] = useFonts({ Jost_400Regular, Jost_600SemiBold })
+  const [user, setUser] = useState()
+
+  async function getUser() {
+    try {
+      const data = await loadUser()
+      setUser(data)
+    } catch (error) {}
+  }
+
+  async function notifications() {
+    const data = await Notifications.getAllScheduledNotificationsAsync()
+    console.log('NOTIFICACOES')
+    console.log(data)
+  }
+
+  useEffect(() => {
+    notifications()
+    getUser()
+  }, [])
+
+  if (!FontsLoaded) {
     return <AppLoading />
+  }
+
+  if (user) {
+    return (
+      <ThemeProvider theme={theme}>
+        <StatusBar style="auto" />
+        <AuthRoutes />
+      </ThemeProvider>
+    )
   }
 
   return (
